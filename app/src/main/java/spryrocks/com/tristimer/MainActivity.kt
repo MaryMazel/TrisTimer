bf6d56eacae06e2bfa1e64d1b2d94ea7eac3d12a
marym <marymazel@outlook.com>
2018-06-20 11:54:55 +0300

    Initial commit

---
+++ app/src/main/java/spryrocks/com/tristimer/MainActivity.kt
@@ -0,0 +1,12 @@
+package spryrocks.com.tristimer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
